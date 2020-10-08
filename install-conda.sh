337d44de30574082d865d3e0c22ace47c33692ba
riccardo scheda <riccardo.scheda@gmail.com>
2020-10-08 15:54:46 +0200

    added script to install python libraries

--- install-conda.sh
+++ install-conda.sh
@@ -5,4 +5,8 @@ sudo apt upgrade --yes
 # Get Miniconda and make it the main Python interpreter
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x Miniconda3-latest-Linux-x86_64.sh
-./Miniconda3-latest-Linux-x86_64.sh
+./Miniconda3-latest-Linux-x86_64.sh 
+rm /home/riccardo/git/ubuntu-configuration/Miniconda3-latest-Linux-x86_64.sh
+
+exec bash -l
+
---
+++ pylibraries.sh
@@ -0,0 +1,5 @@
+yes | conda install spyder 
+yes | conda install pandas
+yes | conda install matplotlib
+yes | conda install networkx 
+yes | conda install scikit-learn
