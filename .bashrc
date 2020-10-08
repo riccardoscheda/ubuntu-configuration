1816aa42dd902784af27061b12ce09beb9288737
riccardo scheda <riccardo.scheda@gmail.com>
2020-10-08 15:13:06 +0200

    ok miniconda installed

---
+++ .bashrc
@@ -0,0 +1,1 @@
+PATH=/home/riccardo/miniconda3/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/riccardo/miniconda/bin
--- install-conda.sh
+++ install-conda.sh
@@ -3,7 +3,6 @@ sudo apt update --yes
 sudo apt upgrade --yes
 
 # Get Miniconda and make it the main Python interpreter
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
-bash ~/miniconda.sh -b -p ~/miniconda 
-rm ~/miniconda.sh
-export PATH=~/miniconda/bin:$PATH
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod +x Miniconda3-latest-Linux-x86_64.sh
+./Miniconda3-latest-Linux-x86_64.sh
