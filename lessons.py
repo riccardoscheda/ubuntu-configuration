11d528b888e486cede7f6c409fe483248f9611d4
riccardoscheda <riccardo.scheda@gmail.com>
2021-02-01 11:22:44 +0100

    mod

--- lessons.py
+++ lessons.py
@@ -7,20 +7,21 @@ import time
 
 r = sr.Recognizer()
 
+seconds = 120
 
 def convert(audio_file):
 	f = sf.SoundFile(audio_file)
 	duration = len(f)/f.samplerate
 	print("seconds = {}".format(duration))
 	print("Converting into text..")
-	for split in [d for d in (datetime.timedelta(seconds=n) for n in range(0,int(duration),10))]:
-		command = "ffmpeg -i " + str(audio_file) + " -v quiet -vn -ss " + str(split) + " -to " + str(split + datetime.timedelta(seconds=10)) + " tmp.wav -y "
+	for split in [d for d in (datetime.timedelta(seconds=n) for n in range(0,int(duration),seconds))]:
+		command = "ffmpeg -i " + str(audio_file) + " -v quiet -vn -ss " + str(split) + " -to " + str(split + datetime.timedelta(seconds=seconds)) + " tmp.wav -y "
 		os.system(command)
 	
 		time.sleep(5)
 		filename = audio_file.split(".")[0] + ".txt"
 		with sr.AudioFile("tmp.wav") as source:
-			audio_text = r.record(source,duration=10)
+			audio_text = r.record(source,duration=seconds)
 			text = r.recognize_google(audio_text,language = "it-IN")
 			text_file = open(filename, "a")
 			print(text)
